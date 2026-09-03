bd11591cd529a045334483eaaa8720fbbb1a3130
Carlos Yakimov <cyakimov@gmail.com>
2026-09-02 22:23:23 -0400

    fix: update tmux to 3.7c

--- nix/modules/tmux.nix
+++ nix/modules/tmux.nix
@@ -1,7 +1,19 @@
-{ self, ... }:
+{ pkgs, self, ... }:
+let
+  tmuxVersion = "3.7c";
+in
 {
   programs.tmux = {
     enable = true;
+    package = pkgs.tmux.overrideAttrs (oldAttrs: {
+      version = tmuxVersion;
+      src = pkgs.fetchurl {
+        url = "https://github.com/tmux/tmux/archive/refs/tags/${tmuxVersion}.tar.gz";
+        hash = "sha256-XnsPUztm5WM+K3Kp1IP5U0o0OrcBHrJiG2MJ37pVPao=";
+      };
+      patches = [ ];
+      configureFlags = oldAttrs.configureFlags ++ [ "--disable-jemalloc" ];
+    });
     extraConfig = builtins.readFile "${self}/config/tmux/tmux.conf";
   };
 }
